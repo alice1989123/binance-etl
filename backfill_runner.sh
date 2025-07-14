4e9b0d3973c9dbbe683bb7834ede9b90249679a6
alice <aliciabasilo.ab@gmail.com>
2025-07-14 12:37:23 +0000

    updated wrapper script to run first main ETL then backfilll

--- backfill_runner.sh
+++ backfill_runner.sh
@@ -4,6 +4,8 @@ set -e  # Exit immediately on error
 echo "🟢 $(date '+%F %T') – Starting ETL script"
 
 # Run Python module with logging streamed to both stdout and /tmp/etl.log
-python -m src.backfill_runner --model_name=LSTMModel 2>&1 | tee /tmp/etl.log
+python -m src.modules.etl_klines 2>&1 | tee /tmp/etl.log
+
+python -m src.backfill_runner 2>&1 | tee /tmp/etl.log
 
 echo "✅ $(date '+%F %T') – Script finished"
